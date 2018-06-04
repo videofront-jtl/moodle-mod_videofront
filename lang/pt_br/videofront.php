23eb3d0f1dca0a4d8268125d480da8d0d9b68217
Eduardo Kraus <kraus@MacNovo-do-kraus.local>
2018-06-04 02:33:22 -0300

    Add PT-BR

---
+++ lang/pt_br/videofront.php
@@ -0,0 +1,46 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package    mod_videofront
+ * @copyright  2018 Eduardo Kraus  {@link http://videofront.com.br}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['modulename'] = 'VideoFront Video';
+$string['modulenameplural'] = 'VideoFront Video';
+$string['modulename_help'] = 'VideoFront is a plugin that simplifies and adds a lot of security in Moodle transactions with the Video Library';
+$string['videofront:addinstance'] = 'Add a new VideoFront';
+$string['videofront:submit'] = 'Submit VideoFront';
+$string['videofront:view'] = 'View VideoFront';
+$string['videofront'] = 'VideoFront';
+$string['pluginadministration'] = 'VideoFront administration';
+$string['pluginname'] = 'VideoFront';
+
+$string['name_title'] = 'Title';
+$string['identifier'] = 'Video identifier';
+$string['identifier_help'] = 'Identifier of the video only in the Video Library!';
+
+$string['url_title'] = 'Video Library Url';
+$string['url_desc'] = 'Add Video URL for Video Sync!';
+
+$string['token_title'] = 'Video Library Token';
+$string['token_desc'] = 'Token for API to communicate with Video Library!';
+
+$string['security_title'] = 'Security';
+$string['security_desc'] = 'About the Player show?';
