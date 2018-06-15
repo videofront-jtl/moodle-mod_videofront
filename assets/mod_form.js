69fa03db5d9d3a424d10bc497b8a06b8b237b73a
Eduardo Kraus <kraus@MacNovo-do-kraus.local>
2018-06-15 02:39:53 -0300

    Code Checker

--- assets/mod_form.js
+++ assets/mod_form.js
@@ -77,7 +77,7 @@ require(['jquery'], function($) {
                     if (video.VIDEO_TIPO == "video") {
                         var videoIdentifier = video.VIDEO_IDENTIFIER.toUpperCase();
                         var html1 = '<div class="list-itens-grid" id="video_identifier_' + videoIdentifier + '" ' +
-                            '             onclick="selectVideo(\'' + videoIdentifier + '\', \'' + video.VIDEO_TITULO + '\')"'>' +
+                            '             onclick="selectVideo(\'' + videoIdentifier + '\', \'' + video.VIDEO_TITULO + '\')" >' +
                             '    <span class="itens" >' +
                             '        <img src="' + linkThumb + '" height="133" width="236"><br>' +
                             '        <span class="title">' + title + '</span>' +
--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_videofront';
 $plugin->version = 2018061402;
-$plugin->release = 'v2.3.10';
+$plugin->release = 'v2.3.11';
 $plugin->requires = 2014051200;
 $plugin->maturity = MATURITY_STABLE;
