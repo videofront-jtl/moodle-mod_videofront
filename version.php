4414b623e04de18c43667f40132ea7b0abeca5d1
videofront-jtl <87443058+videofront-jtl@users.noreply.github.com>
2023-05-16 10:54:33 -0300

    build: update version number

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2023051600; // Version...
+$plugin->version = 2023051601; // Version...
 $plugin->requires = 2014051200;
 $plugin->release = 'v2.4.5';
 $plugin->component = 'mod_videofront';
